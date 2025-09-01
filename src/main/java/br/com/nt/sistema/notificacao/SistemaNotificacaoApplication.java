7118c1209c0ae75d44c6cb8af44b1388e73d70fc
Ediwaldo Neto <ediwaldo.neto@outlook.com>
2025-08-31 21:45:51 -0300

    criando estrutura

---
+++ src/main/java/br/com/nt/sistema/notificacao/SistemaNotificacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.nt.sistema.notificacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaNotificacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaNotificacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/nt/sistema/notificacao/SistemaNotificacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nt.sistema.notificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaNotificacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
